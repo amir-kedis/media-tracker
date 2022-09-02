f8b887fae363d056d4c0bf4f4d0973ea644f2a9b
amir-kedis <amir.kedis@gmail.com>
2022-09-02 19:06:22 +0200

    setup layout template

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("TODO.html")
+
+# auto start flask
+if __name__ == "__main__":
+
+    app.run(debug=True, port=9000)

5decb28e60a8c7aa97d67bc5e531500a884043a8
amir-kedis <amir.kedis@gmail.com>
2022-09-03 11:11:13 +0200

    navbar basic layout

--- app.py
+++ app.py
@@ -22,6 +22,31 @@ def login():
 def logout():
     return render_template("TODO.html")
 
+# login route
+@app.route("/add_media")
+def addMedia():
+    return render_template("TODO.html")
+
+# login route
+@app.route("/edit_list")
+def editList():
+    return render_template("TODO.html")
+
+# login route
+@app.route("/watched")
+def watched():
+    return render_template("TODO.html")
+
+# login route
+@app.route("/watching")
+def watching():
+    return render_template("TODO.html")
+
+# login route
+@app.route("/plan_to_watch")
+def planToWatch():
+    return render_template("TODO.html")
+
 # auto start flask
 if __name__ == "__main__":
 
